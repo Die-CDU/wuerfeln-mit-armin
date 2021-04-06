c346438a3c995cc607e60bace62cbc56310b6283
Der Armin <armin@wuerfelspiele.de>
2021-04-06 18:17:55 +0200

    Fix linting issue

--- app/helpers/is-clipboard-supported.js
+++ app/helpers/is-clipboard-supported.js
@@ -1,5 +1,6 @@
 import { helper } from '@ember/component/helper';
 
-const IS_SUPPORTED = document.queryCommandSupported && document.queryCommandSupported('copy');
+const IS_SUPPORTED =
+  document.queryCommandSupported && document.queryCommandSupported('copy');
 
 export default helper(() => IS_SUPPORTED);
